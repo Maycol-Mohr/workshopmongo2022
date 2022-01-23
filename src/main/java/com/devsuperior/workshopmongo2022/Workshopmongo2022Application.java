78dd41823f926edf8557bbb0da3958a674f06b75
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-01-23 20:19:45 +0100

    Project created

---
+++ src/main/java/com/devsuperior/workshopmongo2022/Workshopmongo2022Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.workshopmongo2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/workshopmongo2022/Workshopmongo2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.workshopmongo2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
