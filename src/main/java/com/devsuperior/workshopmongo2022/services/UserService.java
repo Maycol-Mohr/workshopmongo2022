61a929ff53e6632c45b69291ddad4be12194b4b4
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-01-24 02:12:19 +0100

    GET user by id

--- src/main/java/com/devsuperior/workshopmongo2022/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo2022/services/UserService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.workshopmongo2022.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.workshopmongo2022.models.dto.UserDTO;
 import com.devsuperior.workshopmongo2022.models.entities.User;
 import com.devsuperior.workshopmongo2022.repositories.UserRepository;
+import com.devsuperior.workshopmongo2022.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		return new UserDTO(entity);
 	}
 }
---
+++ src/main/java/com/devsuperior/workshopmongo2022/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.workshopmongo2022.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
